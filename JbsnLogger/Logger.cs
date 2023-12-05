9b3c62e70f60cf56368aa7dd2aad446e481e74c3
Neto Santos <netosantosbatista@outlook.com>
2023-12-05 11:23:24 -0300

    add summary

--- JbsnLogger/Logger.cs
+++ JbsnLogger/Logger.cs
@@ -11,11 +11,17 @@ namespace JbsnLogger
 
         private static string _applicationKey = string.Empty;
 
+        /// <summary>
+        /// Initializes the logger informing the application key as a parameter.
+        /// </summary>
         public static void Initialize(string applicationKey)
         {
             _applicationKey = applicationKey;
         }
 
+        /// <summary>
+        /// Formats and writes an log message.
+        /// </summary>
         public static Task Log(LogLevel logLevel, string message, Exception? exception = null, Guid? eventId = null)
         {
             switch (logLevel)
@@ -27,16 +33,25 @@ namespace JbsnLogger
             }
         }
 
+        /// <summary>
+        /// Formats and writes an error log message.
+        /// </summary>
         public static Task LogError(string message, Exception? exception = null, Guid? eventId = null)
         {
             return Log(LogTypeConstant.Error, message, exception, eventId);
         }
 
+        /// <summary>
+        /// Formats and writes an informational log message.
+        /// </summary>
         public static Task LogInfo(string message, Exception? exception = null, Guid? eventId = null)
         {
             return Log(LogTypeConstant.Info, message, exception, eventId);
         }
 
+        /// <summary>
+        /// Formats and writes an log message.
+        /// </summary>
         private static async Task Log(string logType, string message, Exception? exception = null, Guid? eventId = null)
         {
             ValidateApplicationKey();
@@ -51,6 +66,9 @@ namespace JbsnLogger
             await client.PostAsJsonAsync(_baseAddress, recordDto);
         }
 
+        /// <summary>
+        /// Validates the application key.
+        /// </summary>
         private static void ValidateApplicationKey()
         {
             if (string.IsNullOrEmpty(_applicationKey))
